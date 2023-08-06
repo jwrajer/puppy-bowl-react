38f9e6c5a09aea4ed13d8f795e94cf8204da2ae4
Wyatt Rajer <wyattrajer@gmail.com>
2023-08-06 15:02:08 -0400

    Start framing for player card

--- src/components/DisplaySinglePlayer.jsx
+++ src/components/DisplaySinglePlayer.jsx
@@ -1,12 +1,16 @@
 const DisplaySinglePlayer = ({ singlePlayer }) => {
   return (
-    <>
-      <h1>{singlePlayer.name}</h1>
-      <h2>{singlePlayer.breed}</h2>
-      <h3>{singlePlayer.status}</h3>
-      <img alt='player' src={singlePlayer.imageUrl} />
+    <section className='player-card'>
+      <section className='player-info'>
+        <h1>{singlePlayer.name}</h1>
+        <h2>{singlePlayer.breed}</h2>
+        <h3>{singlePlayer.status}</h3>
+      </section>
+      <section className='player-image'>
+        <img alt='player' src={singlePlayer.imageUrl} />
+      </section>
 
-    </>
+    </section>
   )
 }
 

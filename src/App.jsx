f985975d9f3a41f64c1a7f3360aa4a06288706ae
Wyatt Rajer <wyattrajer@gmail.com>
2023-08-05 21:04:39 -0400

    Recieves player data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+// call api
+// get list of players
+// pass each button details
+// look into routes w uniqie ids
+
 function App() {
+  const [playerList, setPlayerList] = useState([]);
+
+  useEffect(() => {
+    const fetchPlayerList = async() => {
+      try {
+        const response = await fetch (`https://fsa-puppy-bowl.herokuapp.com/api/2306-fsa-et-web-ft/players`);
+        const data = await response.json();
+        setPlayerList(data.data.players);
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchPlayerList();
+  }, [])
 
   return (
     <>
-      <h1>test</h1>
+      <h1>{playerList}</h1>
     </>
   )
 }

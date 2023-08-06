b2df9beb7795fd9909743043ad3034869bce95f6
Wyatt Rajer <wyattrajer@gmail.com>
2023-08-06 14:51:55 -0400

    single player page shows details/info

--- src/components/DisplaySinglePlayer.jsx
+++ src/components/DisplaySinglePlayer.jsx
@@ -2,6 +2,10 @@ const DisplaySinglePlayer = ({ singlePlayer }) => {
   return (
     <>
       <h1>{singlePlayer.name}</h1>
+      <h2>{singlePlayer.breed}</h2>
+      <h3>{singlePlayer.status}</h3>
+      <img alt='player' src={singlePlayer.imageUrl} />
+
     </>
   )
 }

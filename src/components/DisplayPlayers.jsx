ff77518e5e3157cc2ebabc1627b0b255540999c2
Wyatt Rajer <wyattrajer@gmail.com>
2023-08-05 21:31:19 -0400

    Display player list shows pictures w alt text

--- src/components/DisplayPlayers.jsx
+++ src/components/DisplayPlayers.jsx
@@ -1,8 +1,15 @@
 const DisplayPlayers = ({ playerList }) => {
+  console.log(playerList);
   return (
     <>
       {playerList.map((player) => {
-        return <li>{player.name}</li>
+        const altDescription = `picture of ${player.name}`
+        return (
+          <section key={player.name}>
+            <h2>{player.name}</h2>
+            <img alt={altDescription}  src={player.imageUrl}/>
+          </section>
+        )
       })}
     </>
   )
